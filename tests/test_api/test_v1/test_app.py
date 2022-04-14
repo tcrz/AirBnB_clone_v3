22f9688ba9c20d1a2759c2003f550df4cfe2ada1
Tonycrz <tonybaidoo0@gmail.com>
2022-04-14 23:57:12 +0000

    test for 404-error status code handling

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -7,5 +7,13 @@ class TestIntegrations(TestCase):
         self.app = app.test_client()
 
     def test_statusroute(self):
+        """tests the status route returns JSON-formatted
+        response correctly"""
         response = self.app.get('/api/v1/status')
         assert b'{\n  "status": "OK"\n}\n' in response.data
+
+    def test_404error(self):
+        """tests the handing of 404 error returns JSON-formatted
+        status code response"""
+        response = self.app.get('/api/v1/nop')
+        self.assertEqual(b'{\n  "error": "Not found"\n}\n', response.data)
