40e3770217f2d2375cbf2a6a94571fab62324e5e
Tonycrz <tonybaidoo0@gmail.com>
2022-04-15 11:00:02 +0000

    test stats route

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from api.v1.app import app
+from models import storage
 
 
 class TestIntegrations(TestCase):
@@ -16,19 +17,26 @@ class TestIntegrations(TestCase):
         assert b'{\n  "status": "OK"\n}\n' in response.data
         self.assertEqual(response.status_code, 200)
 
-    # def test_404error(self):
-    #     """tests the handing of 404 error returns JSON-formatted
-    #     status code response"""
-    #     response = self.app.get('/api/v1/nop')
-    #     self.assertEqual(b'{\n  "error": "Not found"\n}\n', response.data)
-    #     self.assertEqual(response.status_code, 404)
+    def test_404error(self):
+        """tests the handing of 404 error returns JSON-formatted
+        status code response"""
+        response = self.app.get('/api/v1/nop')
+        self.assertEqual(b'{\n  "error": "Not found"\n}\n', response.data)
+        self.assertEqual(response.status_code, 404)
+
+    def test_stat(self):
+        """tests the stats route"""
+        response = self.app.get('/api/v1/stats')
+        data = response.data
+        self.assertTrue(data in response.data)
+        self.assertEqual(response.status_code, 200)
 
     # States view
     def test_get_states(self):
         """tests that all state objects are returned in JSON"""
         response = self.app.get('api/v1/states')
         self.assertEqual(response.status_code, 200)
-        print(response.data)
+        # print(response.data)
 
 
 if __name__ == '__main__':
