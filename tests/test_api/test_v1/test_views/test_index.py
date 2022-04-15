e2d82cf51c237d8dada4eb749ac33fc8f3699152
Tonycrz <tonybaidoo0@gmail.com>
2022-04-15 15:09:31 +0000

    add tests

---
+++ tests/test_api/test_v1/test_views/test_index.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from api.v1.app import app
+
+
+class TestIntegrations(TestCase):
+    def setUp(self):
+        """set up app for testing"""
+        self.app = app.test_client()
+
+    # Index view
+    def test_statusroute(self):
+        """tests the status route returns JSON-formatted
+        response correctly"""
+        response = self.app.get('/api/v1/status')
+        assert b'{\n  "status": "OK"\n}\n' in response.data
+        self.assertEqual(response.status_code, 200)
+
+    def test_404error(self):
+        """tests the handing of 404 error returns JSON-formatted
+        status code response"""
+        response = self.app.get('/api/v1/nop')
+        self.assertEqual(b'{\n  "error": "Not found"\n}\n', response.data)
+        self.assertEqual(response.status_code, 404)
